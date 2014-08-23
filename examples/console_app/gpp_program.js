8ae2c76c78bf703f344fcb13279e6bd4cafdd941
Paweł Tymura <paweltymura@op.pl>
2014-08-23 22:26:22 +0200

    Added examples folder, changed that we now use arguments from native node, moved some libs from devDependencies to dependencies

---
+++ examples/console_app/gpp_program.js
@@ -0,0 +1,17 @@
+﻿#!/usr/local/bin/node
+
+var args = process.argv.slice(2),
+    Parser = require('../../lib/GuitarProParser.js'),
+    fs = require('fs');
+
+if (args.length == 0)
+    console.log("Usage: gpp_program.js [arguments] \narguments - paths to gp4 files.");
+
+args.map( function(file_path){
+    var data = fs.readFileSync(file_path),
+        parser = new Parser(data),
+        parsed = parser.parse();
+
+    console.log("\n\n");
+    console.dir(parsed);
+});

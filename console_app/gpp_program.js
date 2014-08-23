810857455eb9c12ba20c3625f377582b5013b595
Paweł Tymura <paweltymura@op.pl>
2014-08-23 07:54:27 +0200

    Move server files to other path. Added console app. Added new library to argv

---
+++ console_app/gpp_program.js
@@ -0,0 +1,14 @@
+﻿#!/usr/local/bin/node
+
+var argv = require("optimist")
+    .usage('Retreive information from gp4 file.\nUsage: $0 -f filename.gp4')
+    .demand("f")
+    .argv,
+Parser = require('../lib/GuitarProParser.js'),
+fs = require('fs');
+ 
+var data = fs.readFileSync(argv.f),
+    parser = new Parser(data),
+    parsed = parser.parse();
+
+console.dir(parsed.header);
